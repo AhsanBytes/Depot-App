289e75bbb644092c48ced969d8859cb144837404
Ahsan Anwar <ahsananwar190@gmail.com>
2024-08-22 19:23:06 +0500

    add length validation in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, format: { with: %r{\.(gif|jpg|png)\z}i, message: 'must be a GIF, JPG or PNG Image.'}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
