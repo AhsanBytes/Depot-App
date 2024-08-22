53a4c2643c9d5aad22b7781e15d8809cb23f03e7
Ahsan Anwar <ahsananwar190@gmail.com>
2024-08-22 19:17:39 +0500

    scaffold, model validation and model test with rspecs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, :price, presence: true
+  validates :title, uniqueness: true
+  validates :image_url, format: { with: %r{\.(gif|jpg|png)\z}i, message: 'must be a GIF, JPG or PNG Image.'}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
