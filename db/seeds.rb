e8f79ffc89d68cbe711c5cf59d63a72d167da8dc
Ahsan Anwar <ahsananwar190@gmail.com>
2024-09-03 11:12:01 +0500

    deployment and production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Product.delete_all
 # . . .
-Product.create!(title: 'Design and Build Great Web APIs',
- description:
- %{<p>
+Product.create!(title: "Design and Build Great Web APIs",
+                description: %{<p>
  <em>Robust, Reliable, and Resilient</em>
  APIs are transforming the business world at an increasing pace. Gain
  the essential skills needed to quickly design, build, and deploy
@@ -21,6 +20,8 @@ Product.create!(title: 'Design and Build Great Web APIs',
  open-source utilities and common programming patterns you can apply
  right away.
  </p>},
- image_url: 'maapis.jpg',
- price: 24.95)
+                image_url: "maapis.jpg",
+                price: 24.95)
+
+User.create! name: "dave", password: Rails.application.credentials.dave_password
 # . . .
